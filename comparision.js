42fbb37c013f52066deab99382bb48645749a0e5
prajwal <prajwalshah912@gmail.com>
2023-04-21 00:14:03 +0530

    destructing, array, position,error, memory location of array

--- comparision.js
+++ comparision.js
@@ -28,3 +28,65 @@
 
 //nan  -- example : ""/number it throws NAN
 // console.log(typeof NaN)
+
+
+// let data = 10- -10;
+// console.log(data); //20 ans beacouse - - is always +
+
+//set, set weak, set map, map - is an one type of data type 
+//it is used to remove the duplicate elements
+// let set = new Set([1,1,2,2,3,5,7,90]);
+// console.log(set)
+
+
+// let data = {name:'Anil'}
+// console.log(delete data.name)// it is delete that;s why it is true
+// console.log(data)//it is deleted so the ans is Empty obj
+
+
+// let data = {name:'Anil'};
+// console.log(data)// ans:false; the data ko directly delete nhikr skte hai if any property hai toh delete kr skte hai
+
+// let data = ["peter", "hey", "ash"];
+// let [y] = data;
+// let [z,r] = data;
+// let [i,o,q] = data;
+// console.log(y)//ans: peter;
+// console.log(z,r)//ans: peter, hey;
+// console.log(i,o,q)//ans: peter,hey,ash;
+
+// let data = ["peter", "hey", "ash"];
+// let [, ,y] = data; //we can get the 2 value and if the 2 ,, then the 3 value
+// console.log(y)//ans: hey;
+
+
+// print name without using . 
+// const data = {name:"prajwal",agh:8}
+// const {name} = data
+// console.log(name)
+
+
+//merge with destructing
+// const data = {name:"prajwal",age:8, skill:"js"}
+// const info = {city:"delhi",mail:"mail@gmail.com"}
+// let d =  {...data, ...info}
+// 
+// console.log(d)
+
+
+// const data = {name:"prajwal",age:8, skill:"js"}
+// const info = {city:"delhi",mail:"mail@gmail.com"}
+// let d =  {data, ...info}
+// 
+// console.log(d)
+
+
+// const data = {name:"prajwal",age:8, skill:"js"}
+// const info = {city:"delhi",mail:"mail@gmail.com", skill:"react"}
+// let d =  {...data, ...info}
+// it will show the second val of skill and but the position will be the first one
+// console.log(d)
+
+
+// let name = "prajwal";
+// console.log(name())//it will throw error
