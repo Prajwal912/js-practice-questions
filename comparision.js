77fb45f13410481f44603437fe17a47bde2c399e
prajwal <prajwalshah912@gmail.com>
2023-04-20 23:16:01 +0530

    js easy interview questions

---
+++ comparision.js
@@ -0,0 +1,30 @@
+// let a = [];
+// let b = [];
+//both come false because we can't compare both array beacuse of the memory location 
+//both array have different memory location  ans:false in both
+// console.log(a==b)
+// console.log(a===b)
+
+
+// let c = [];
+// let d = c;
+///here both has same memory location because d is now having the value of c ans:true in both
+// console.log(c==d)
+// console.log(c===d)
+
+
+// let a = [20];
+// let b = [20];
+// here only the value of the 0th postion is checing that;s why it is coming true in both 
+// console.log(a[0] == b[0])
+// console.log(a[0] === b[0])
+
+
+
+//array destructing
+// let z = [1,2,3,4];
+// console.log(...z); 
+
+
+//nan  -- example : ""/number it throws NAN
+// console.log(typeof NaN)
