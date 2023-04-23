d2513b82891484f749f128cc2175f34f3b4ed2ad
prajwal <prajwalshah912@gmail.com>
2023-04-23 23:54:44 +0530

    operators, promise, parse

---
+++ practice_2.js
@@ -0,0 +1,28 @@
+// let res = false||{}||null;
+// console.log(res)
+//ans because || always takes the truthy value to check truthy val
+// if({}){
+    // console.log("yes")
+// }
+
+//if 3 to val false hai toh last wala val leleta hai
+// let res = null||false||'';
+// console.log(res)
+
+// let res = ""||false||null;
+// console.log(res)
+
+// let res = [] || 0 || true;
+// console.log(res)
+
+// if([]){
+// console.log("yes")
+// }
+
+
+// console.log(Promise.resolve(5))//ans is promise is fullfilled : 5;
+
+
+// console.log("♥" === "♥")//cause of behind of every emoji there is unicode so here  both unicode is same that's why true
+
+// JSON.parse();//JSON data ko js value mei convert krta hai
