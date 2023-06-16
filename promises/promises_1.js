a451b809ad918d7414ffc6b99652a80d67bfdb5b
prajwal <prajwalshah912@gmail.com>
2023-06-16 23:20:47 +0530

    setTimeout and setInterval

--- promises/promises_1.js
+++ promises/promises_1.js
@@ -5,13 +5,32 @@
 // },5000)
 
 
-let count = 0
+// let count = 0
 
-let timee = setInterval(() => {
-    console.log("I am a synchronous message")
-    count += 1
-    if(count >= 5){
-        clearInterval(timee)
-    }
-}, 4000)
-console.log("I am a synchronous message,I am a synchronous message")
+// let timee = setInterval(() => {
+//     console.log("I am a synchronous message")
+//     count += 1
+//     if(count >= 5){
+//         clearInterval(timee)
+//     }
+// }, 4000)
+// console.log("I am a synchronous message,I am a synchronous message")
+
+
+
+// callback1 must be called only one time, after 2 seconds.
+// callback2 must be called three times with an interval of 1 second.
+
+setTimeout(() => {
+    console.log("callback1")
+
+}, 2000)
+
+let count = 0;
+let sett =  setInterval(() => {
+    count += 1;
+    console.log("callback2")
+   if(count === 3){
+       clearInterval(sett);
+   }
+ },1000)
