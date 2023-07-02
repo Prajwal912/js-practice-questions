92d1f1ea95e539451f5618a0ff1a1df02fd9b5e5
prajwal <prajwalshah912@gmail.com>
2023-07-02 20:50:45 +0530

    filter done

---
+++ array builtin methods/array_eith_empty_slots/filter.js
@@ -0,0 +1,7 @@
+let words = ['spray', 'limit', 'elite', 'exuberant', 'destruction', 'present']
+console.log(words.length);
+
+let res = words.filter((word) => 
+  word.length > 6
+  )
+  console.log(res)
