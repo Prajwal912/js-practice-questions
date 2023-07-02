6fadd1fe5ca8883f627d63f8435dabe1c2a19b78
prajwal <prajwalshah912@gmail.com>
2023-07-02 20:59:48 +0530

    flat

---
+++ array builtin methods/array_eith_empty_slots/flat.js
@@ -0,0 +1,15 @@
+const arr1 = [0, 1, 2, [3, 4]];
+console.log(arr1.flat())
+
+
+const arr2 = [0, 1, 2,  [[[3, 4]]]];
+console.log(arr2.flat(3))
+
+const arr4 = [1, 2, [3, 4, [5, 6, [7, 8, [9, 10]]]]];
+console.log(arr4.flat(Infinity))
+
+//it also removes the empty slots
+const array2 = [1, , 3, ["a", , ["d", , "e"]]];
+console.log(array2.flat(2))
+
+
