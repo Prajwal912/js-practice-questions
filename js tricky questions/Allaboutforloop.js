b20029d7b6a41b055cec0f4f190390b3e14065b1
prajwal1221 <prajwalshah912@gmail.com>
2023-07-11 17:28:11 +0530

    right angle triangle withb repeat method

--- js tricky questions/Allaboutforloop.js
+++ js tricky questions/Allaboutforloop.js
@@ -1,17 +1,22 @@
 // for in loop -- iterates over the strings properties of an object
 
-let obj = {
-    a:'1',
-    b:'2',
-    c:'3',
-}
-for(let property in obj){
-//  console.log(`${property}: ${obj[property]}`)
-}
+// let obj = {
+//     a:'1',
+//     b:'2',
+//     c:'3',
+// }
+// for(let property in obj){
+// //  console.log(`${property}: ${obj[property]}`)
+// }
 
 
-//for of --> it iterates over a value
-let obj1 = [1,4,67,9,0]
-for(let property of obj1){
-//  console.log(`${property}`)
-}
+// //for of --> it iterates over a value
+// let obj1 = [1,4,67,9,0]
+// for(let property of obj1){
+// //  console.log(`${property}`)
+// }
+
+// for (let i = 1; i<=5; i++) {
+//   console.log('*'.repeat(i))  
+// }
+
